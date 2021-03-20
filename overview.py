861d01175312ace94b36b7a58066a5a06e2c075a
Grant Barnes <gfbarnes78@gmail.com>
2021-03-19 20:41:27 -0500

    adding python script to show overview of linux distro

---
+++ overview.py
@@ -0,0 +1,132 @@
+import os
+import subprocess
+
+
+def has_command(command):
+    return os.system("command -v " + command + " >/dev/null 2>&1") == 0
+
+
+def run_command(command):
+    return subprocess.check_output(['bash', '-c', command]).decode("utf-8").strip()
+
+
+def get_distro():
+    for fileName in ["/etc/lsb-release", "/usr/lib/os-release", "/etc/os-release"]:
+        file = open(fileName, "r")
+        for line in file:
+            if line.startswith('PRETTY_NAME="') or line.startswith('DISTRIB_DESCRIPTION="'):
+                return line.split('"')[1]
+    return "(Unknown)"
+
+
+def get_uptime():
+    boot = int(run_command('date -d"$(uptime -s)" +%s'))
+    now = int(run_command("date +%s"))
+    uptimeSeconds = now - boot
+
+    secondsCount = int(uptimeSeconds % 60)
+    minutesCount = int(uptimeSeconds / 60 % 60)
+    hoursCount = int(uptimeSeconds / 60 / 60 % 24)
+    daysCount = int(uptimeSeconds / 60 / 60 / 24 % 365)
+    yearsCount = int(uptimeSeconds / 60 / 60 / 24 / 365)
+
+    seconds = str(secondsCount) + " seconds"
+    minutes = str(minutesCount) + " minutes"
+    hours = str(hoursCount) + " hours"
+    days = str(daysCount) + " days"
+    years = str(yearsCount) + " years"
+
+    if secondsCount == 1:
+        seconds = seconds[:-1]
+    if minutesCount == 1:
+        minutes = minutes[:-1]
+    if hoursCount == 1:
+        hours = hours[:-1]
+    if daysCount == 1:
+        days = days[:-1]
+    if yearsCount == 1:
+        years = years[:-1]
+
+    uptime = ""
+    if yearsCount != 0:
+        uptime += years + ", "
+    if daysCount != 0:
+        uptime += days + ", "
+    if hoursCount != 0:
+        uptime += hours + ", "
+    if minutesCount != 0:
+        uptime += minutes + ", "
+    uptime += seconds
+    return uptime
+
+
+def get_packages():
+    dpkg = "0"
+    dnf = "0"
+    pacman = "0"
+
+    snap = "0"
+    flatpak = "0"
+
+    if has_command("dpkg"):
+        dpkg = run_command("dpkg --list | wc -l")
+    if has_command("dnf"):
+        dnf = run_command("dnf list installed | wc -l")
+    if has_command("pacman"):
+        pacman = run_command("pacman -Q | wc -l")
+
+    if has_command("snap"):
+        snap = run_command("snap list | wc -l")
+    if has_command("flatpak"):
+        flatpak = run_command("flatpak list | wc -l")
+
+    packages = ""
+    if dpkg != "0":
+        packages += dpkg + " (dpkg), "
+    if dnf != "0":
+        packages += dnf + " (dnf), "
+    if pacman != "0":
+        packages += pacman + " (pacman), "
+
+    if snap != "0":
+        packages += snap + " (snap), "
+    if flatpak != "0":
+        packages += flatpak + " (flatpak), "
+
+    packages = packages[:-2]
+    return packages
+
+
+def get_memory():
+    file = open("/proc/meminfo", "r")
+    total = 0
+    available = 0
+    for line in file:
+        if line.startswith("MemTotal"):
+            total = int(line.split(" ")[-2]) // 1024
+        if line.startswith("MemAvailable"):
+            available = int(line.split(" ")[-2]) // 1024
+        if total and available:
+            break
+
+    used = total - available
+    percent = (used * 100) // total
+    return str(used) + " MB / " + str(total) + " MB (" + str(percent) + "%)"
+
+
+def get_cpu():
+    file = open("/proc/cpuinfo", "r")
+    for line in file:
+        if line.startswith("model name"):
+            return line.split(": ")[1].strip()
+    return "(Unknown)"
+
+
+print(run_command("echo $USER") + "@" + run_command("echo $HOSTNAME"))
+print("-------------------------------")
+print("  Distro: " + get_distro())
+print("  Kernel: " + run_command("uname -srm"))
+print("  Uptime: " + get_uptime())
+print("Packages: " + get_packages())
+print("     CPU: " + get_cpu())
+print("  Memory: " + get_memory())
