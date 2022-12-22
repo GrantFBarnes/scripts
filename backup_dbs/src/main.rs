b525ff7e7d280053a846f13f4a17638385504488
Grant Barnes <gfbarnes78@gmail.com>
2022-12-21 18:33:19 -0600

    limiting database backups to most recent 30

--- backup_dbs/src/main.rs
+++ backup_dbs/src/main.rs
@@ -1,5 +1,8 @@
 use chrono::prelude::Local;
+use regex::Regex;
 use std::env::VarError;
+use std::ffi::OsString;
+use std::fs::{DirEntry, ReadDir};
 use std::io::Result;
 use std::process::{Command, Output};
 use std::string::FromUtf8Error;
@@ -30,9 +33,52 @@ fn write_to_file(path: &String, contents: String) {
     fs::write(path, contents).expect("Failed to write backup to file");
 }
 
+fn remove_file(path: &String) {
+    match fs::remove_file(path) {
+        _ => (),
+    }
+}
+
 fn create_directory(path: &String) {
     match fs::create_dir_all(path) {
         _ => (),
+    }
+}
+
+fn get_backup_files(path: &String) -> Vec<String> {
+    let mut backup_files: Vec<String> = vec![];
+
+    let file_name_regex = Regex::new(r"^\d{4}-\d{2}-\d{2}-backup-.*\.sql$").unwrap();
+    let dir: Result<ReadDir> = fs::read_dir(path);
+    if dir.is_ok() {
+        let dir: ReadDir = dir.unwrap();
+        for entry in dir {
+            if entry.is_ok() {
+                let entry: DirEntry = entry.unwrap();
+                let file_name: OsString = entry.file_name();
+                let file_name: Option<&str> = file_name.to_str();
+                if file_name.is_some() {
+                    let file_name: &str = file_name.unwrap();
+                    if file_name_regex.is_match(file_name) {
+                        backup_files.push(file_name.to_string());
+                    }
+                }
+            }
+        }
+    }
+
+    backup_files.sort();
+    backup_files.reverse();
+    backup_files
+}
+
+fn cleanup_backups(backup_files: Vec<String>, db_backup_dir: &String) {
+    const COUNT: usize = 30;
+    if backup_files.len() > COUNT {
+        let files_to_remove: &[String] = &backup_files[COUNT..];
+        for file in files_to_remove {
+            remove_file(&format!("{}/{}", db_backup_dir, file));
+        }
     }
 }
 
@@ -64,5 +110,8 @@ fn main() {
             let backup: String = backup.unwrap();
             write_to_file(&file_name, backup);
         }
+
+        let backup_files: Vec<String> = get_backup_files(&db_backup_dir);
+        cleanup_backups(backup_files, &db_backup_dir);
     }
 }
