4768fe2ce45a2073c084c9d4ead7eeca5059f46b
Grant Barnes <gfbarnes78@gmail.com>
2024-01-30 20:59:29 -0600

    linux_setup reorganize main file methods

--- linux_setup/src/main.rs
+++ linux_setup/src/main.rs
@@ -27,65 +27,203 @@ enum InstallMethod {
     Cancel,
 }
 
-fn repository_setup(distribution: &mut Distribution) -> Result<(), io::Error> {
-    distribution.setup()?;
-    if distribution.packages.contains("flatpak") {
-        flatpak::setup(distribution)?;
+fn main() -> Result<(), io::Error> {
+    let mut distribution = distribution::Distribution::new()?;
+    run_menu(0, &mut distribution)
+}
+
+fn run_menu(start_idx: usize, distribution: &mut Distribution) -> Result<(), io::Error> {
+    let mut options: Vec<&str> = vec!["Repository Setup"];
+    if distribution
+        .desktop_environments
+        .contains(&DesktopEnvironment::Gnome)
+    {
+        options.push("GNOME Setup");
     }
+    if distribution
+        .desktop_environments
+        .contains(&DesktopEnvironment::KDE)
+    {
+        options.push("KDE Setup");
+    }
+    options.push("Update Packages");
+    options.push("Auto Remove Packages");
+    options.push("Install Packages");
+    options.push("Exit");
+
+    let selection = Select::new()
+        .title("Linux Setup")
+        .options(&options)
+        .default_index(start_idx)
+        .erase_after(true)
+        .run_select()?;
+    if selection.is_none() {
+        return Ok(());
+    }
+    let selection: usize = selection.unwrap().0;
+
+    match options[selection] {
+        "Repository Setup" => repository_setup(distribution)?,
+        "GNOME Setup" => gnome::setup(distribution)?,
+        "KDE Setup" => kde::setup()?,
+        "Update Packages" => {
+            distribution.update()?;
+            if distribution.packages.contains("flatpak") {
+                flatpak::update()?;
+            }
+            if distribution.packages.contains("snapd") {
+                snap::update()?;
+            }
+            other::update(distribution)?;
+        }
+        "Auto Remove Packages" => {
+            distribution.auto_remove()?;
+            if distribution.packages.contains("flatpak") {
+                flatpak::auto_remove()?;
+            }
+        }
+        "Install Packages" => run_menu_install_packages(0, distribution)?,
+        "Exit" => return Ok(()),
+        _ => (),
+    }
+
+    run_menu(selection + 1, distribution)
+}
+
+fn run_menu_install_packages(
+    start_idx: usize,
+    distribution: &mut Distribution,
+) -> Result<(), io::Error> {
+    let mut options_display: Vec<&str> = vec![];
+    let mut options_value: Vec<&Category> = vec![];
+    for category in Category::iterator() {
+        options_display.push(category.as_str());
+        options_value.push(category);
+    }
+    options_display.push("Exit");
+
+    let selection = Select::new()
+        .title("Choose a Category")
+        .options(&options_display)
+        .default_index(start_idx)
+        .erase_after(true)
+        .run_select()?;
+    if selection.is_none() {
+        return Ok(());
+    }
+    let selection = selection.unwrap().0;
+    match options_display[selection] {
+        "Exit" => return Ok(()),
+        _ => run_menu_category_select(options_value[selection], 0, false, distribution)?,
+    }
+
+    run_menu_install_packages(selection + 1, distribution)
+}
+
+fn run_menu_category_select(
+    category: &Category,
+    start_idx: usize,
+    show_all_desktop_environments: bool,
+    distribution: &mut Distribution,
+) -> Result<(), io::Error> {
+    let mut options_display: Vec<String> = vec![];
+    let mut options_value: Vec<Package> = vec![];
+
+    let mut missing_desktop_environment: bool = false;
+
+    for package in package::get_category_packages(category) {
+        if !distribution.is_available(&package)
+            && !flatpak::is_available(&package)
+            && !snap::is_available(&package)
+            && !other::is_available(&package)
+        {
+            continue;
+        }
+
+        let mut missing_pkg_desktop_environment: bool = false;
+
+        if let Some(de) = &package.desktop_environment {
+            if !distribution.desktop_environments.contains(de) {
+                missing_desktop_environment = true;
+                if !show_all_desktop_environments && !is_installed(&package, distribution) {
+                    continue;
+                }
+                missing_pkg_desktop_environment = true;
+            }
+        }
+
+        options_display.push(format!(
+            "{} ({})",
+            helper::get_colored_string(
+                package.name,
+                if missing_pkg_desktop_environment {
+                    Color::Yellow
+                } else {
+                    Color::White
+                }
+            ),
+            get_install_method(&package, distribution)
+        ));
+        options_value.push(package);
+    }
+
+    if missing_desktop_environment {
+        options_display.reverse();
+        options_display.push(format!(
+            "[{} Uninstalled Desktop Environments]",
+            if show_all_desktop_environments {
+                helper::get_colored_string("Hide", Color::Yellow)
+            } else {
+                helper::get_colored_string("Show", Color::Cyan)
+            }
+        ));
+        options_display.reverse();
+
+        options_value.reverse();
+        options_value.push(Package::new());
+        options_value.reverse();
+    }
+
+    options_display.push(String::from("Exit"));
+    options_value.push(Package::new());
+
+    let selection = Select::new()
+        .title(format!("Category: {}", category.as_str()))
+        .options(&options_display)
+        .default_index(start_idx)
+        .erase_after(true)
+        .run_select()?;
+    if selection.is_none() {
+        return Ok(());
+    }
+    let selection: usize = selection.unwrap().0;
+
+    if missing_desktop_environment && selection == 0 {
+        // toggle show all desktop environments
+        run_menu_category_select(
+            category,
+            selection,
+            !show_all_desktop_environments,
+            distribution,
+        )?;
+    } else if selection < options_value.len() - 1 {
+        // not exit
+        run_menu_package_select(&options_value[selection], distribution)?;
+        run_menu_category_select(
+            category,
+            selection + 1,
+            show_all_desktop_environments,
+            distribution,
+        )?;
+    }
+
     Ok(())
 }
 
-fn run_flatpak_remote_select(
+fn run_menu_package_select(
     package: &Package,
     distribution: &mut Distribution,
 ) -> Result<(), io::Error> {
-    let mut options: Vec<&str> = vec![];
-    if let Some(fp) = &package.flatpak {
-        for remote in &fp.remotes {
-            options.push(remote);
-        }
-    }
-    options.push("Cancel");
-
-    if let Some(remote) = Select::new()
-        .title(format!("Flatpak Remote: {}", package.name))
-        .options(&options)
-        .erase_after(true)
-        .run_select()?
-    {
-        if remote.1 == "Cancel" {
-            return Ok(());
-        }
-        flatpak::install(package, remote.1, distribution)
-    } else {
-        return Ok(());
-    }
-}
-
-fn get_install_method(package: &Package, distribution: &Distribution) -> String {
-    if distribution.is_installed(package) {
-        return helper::get_colored_string("Repository", Color::Green);
-    }
-    if flatpak::is_installed(package, distribution) {
-        return helper::get_colored_string("Flatpak", Color::Blue);
-    }
-    if snap::is_installed(package, distribution) {
-        return helper::get_colored_string("Snap", Color::Magenta);
-    }
-    if other::is_installed(package, distribution) {
-        return helper::get_colored_string("Other", Color::Yellow);
-    }
-    return helper::get_colored_string("Uninstalled", Color::Red);
-}
-
-fn is_installed(package: &Package, distribution: &Distribution) -> bool {
-    distribution.is_installed(package)
-        || flatpak::is_installed(package, distribution)
-        || snap::is_installed(package, distribution)
-        || other::is_installed(package, distribution)
-}
-
-fn run_package_select(package: &Package, distribution: &mut Distribution) -> Result<(), io::Error> {
     let mut options_display: Vec<String> = vec![];
     let mut options_value: Vec<InstallMethod> = vec![];
 
@@ -171,7 +309,7 @@ fn run_package_select(package: &Package, distribution: &mut Distribution) -> Res
     }
     match method {
         InstallMethod::Repository => distribution.install(package)?,
-        InstallMethod::Flatpak => run_flatpak_remote_select(package, distribution)?,
+        InstallMethod::Flatpak => run_menu_flatpak_remote_select(package, distribution)?,
         InstallMethod::Snap => snap::install(package, distribution)?,
         InstallMethod::Other => other::install(package, distribution)?,
         _ => (),
@@ -182,195 +320,60 @@ fn run_package_select(package: &Package, distribution: &mut Distribution) -> Res
     Ok(())
 }
 
-fn run_category_select(
-    category: &Category,
-    start_idx: usize,
-    show_all_desktop_environments: bool,
+fn run_menu_flatpak_remote_select(
+    package: &Package,
     distribution: &mut Distribution,
 ) -> Result<(), io::Error> {
-    let mut options_display: Vec<String> = vec![];
-    let mut options_value: Vec<Package> = vec![];
-
-    let mut missing_desktop_environment: bool = false;
-
-    for package in package::get_category_packages(category) {
-        if !distribution.is_available(&package)
-            && !flatpak::is_available(&package)
-            && !snap::is_available(&package)
-            && !other::is_available(&package)
-        {
-            continue;
+    let mut options: Vec<&str> = vec![];
+    if let Some(fp) = &package.flatpak {
+        for remote in &fp.remotes {
+            options.push(remote);
         }
-
-        let mut missing_pkg_desktop_environment: bool = false;
-
-        if let Some(de) = &package.desktop_environment {
-            if !distribution.desktop_environments.contains(de) {
-                missing_desktop_environment = true;
-                if !show_all_desktop_environments && !is_installed(&package, distribution) {
-                    continue;
-                }
-                missing_pkg_desktop_environment = true;
-            }
-        }
-
-        options_display.push(format!(
-            "{} ({})",
-            helper::get_colored_string(
-                package.name,
-                if missing_pkg_desktop_environment {
-                    Color::Yellow
-                } else {
-                    Color::White
-                }
-            ),
-            get_install_method(&package, distribution)
-        ));
-        options_value.push(package);
     }
+    options.push("Cancel");
 
-    if missing_desktop_environment {
-        options_display.reverse();
-        options_display.push(format!(
-            "[{} Uninstalled Desktop Environments]",
-            if show_all_desktop_environments {
-                helper::get_colored_string("Hide", Color::Yellow)
-            } else {
-                helper::get_colored_string("Show", Color::Cyan)
-            }
-        ));
-        options_display.reverse();
-
-        options_value.reverse();
-        options_value.push(Package::new());
-        options_value.reverse();
-    }
-
-    options_display.push(String::from("Exit"));
-    options_value.push(Package::new());
-
-    let selection = Select::new()
-        .title(format!("Category: {}", category.as_str()))
-        .options(&options_display)
-        .default_index(start_idx)
+    if let Some(remote) = Select::new()
+        .title(format!("Flatpak Remote: {}", package.name))
+        .options(&options)
         .erase_after(true)
-        .run_select()?;
-    if selection.is_none() {
+        .run_select()?
+    {
+        if remote.1 == "Cancel" {
+            return Ok(());
+        }
+        flatpak::install(package, remote.1, distribution)
+    } else {
         return Ok(());
     }
-    let selection: usize = selection.unwrap().0;
+}
 
-    if missing_desktop_environment && selection == 0 {
-        // toggle show all desktop environments
-        run_category_select(
-            category,
-            selection,
-            !show_all_desktop_environments,
-            distribution,
-        )?;
-    } else if selection < options_value.len() - 1 {
-        // not exit
-        run_package_select(&options_value[selection], distribution)?;
-        run_category_select(
-            category,
-            selection + 1,
-            show_all_desktop_environments,
-            distribution,
-        )?;
+fn repository_setup(distribution: &mut Distribution) -> Result<(), io::Error> {
+    distribution.setup()?;
+    if distribution.packages.contains("flatpak") {
+        flatpak::setup(distribution)?;
     }
-
     Ok(())
 }
 
-fn run_install_packages(
-    start_idx: usize,
-    distribution: &mut Distribution,
-) -> Result<(), io::Error> {
-    let mut options_display: Vec<&str> = vec![];
-    let mut options_value: Vec<&Category> = vec![];
-    for category in Category::iterator() {
-        options_display.push(category.as_str());
-        options_value.push(category);
-    }
-    options_display.push("Exit");
-
-    let selection = Select::new()
-        .title("Choose a Category")
-        .options(&options_display)
-        .default_index(start_idx)
-        .erase_after(true)
-        .run_select()?;
-    if selection.is_none() {
-        return Ok(());
-    }
-    let selection = selection.unwrap().0;
-    match options_display[selection] {
-        "Exit" => return Ok(()),
-        _ => run_category_select(options_value[selection], 0, false, distribution)?,
-    }
-
-    run_install_packages(selection + 1, distribution)
+fn is_installed(package: &Package, distribution: &Distribution) -> bool {
+    distribution.is_installed(package)
+        || flatpak::is_installed(package, distribution)
+        || snap::is_installed(package, distribution)
+        || other::is_installed(package, distribution)
 }
 
-fn run_menu(start_idx: usize, distribution: &mut Distribution) -> Result<(), io::Error> {
-    let mut options: Vec<&str> = vec!["Repository Setup"];
-    if distribution
-        .desktop_environments
-        .contains(&DesktopEnvironment::Gnome)
-    {
-        options.push("GNOME Setup");
+fn get_install_method(package: &Package, distribution: &Distribution) -> String {
+    if distribution.is_installed(package) {
+        return helper::get_colored_string("Repository", Color::Green);
     }
-    if distribution
-        .desktop_environments
-        .contains(&DesktopEnvironment::KDE)
-    {
-        options.push("KDE Setup");
+    if flatpak::is_installed(package, distribution) {
+        return helper::get_colored_string("Flatpak", Color::Blue);
     }
-    options.push("Update Packages");
-    options.push("Auto Remove Packages");
-    options.push("Install Packages");
-    options.push("Exit");
-
-    let selection = Select::new()
-        .title("Linux Setup")
-        .options(&options)
-        .default_index(start_idx)
-        .erase_after(true)
-        .run_select()?;
-    if selection.is_none() {
-        return Ok(());
+    if snap::is_installed(package, distribution) {
+        return helper::get_colored_string("Snap", Color::Magenta);
     }
-    let selection: usize = selection.unwrap().0;
-
-    match options[selection] {
-        "Repository Setup" => repository_setup(distribution)?,
-        "GNOME Setup" => gnome::setup(distribution)?,
-        "KDE Setup" => kde::setup()?,
-        "Update Packages" => {
-            distribution.update()?;
-            if distribution.packages.contains("flatpak") {
-                flatpak::update()?;
-            }
-            if distribution.packages.contains("snapd") {
-                snap::update()?;
-            }
-            other::update(distribution)?;
-        }
-        "Auto Remove Packages" => {
-            distribution.auto_remove()?;
-            if distribution.packages.contains("flatpak") {
-                flatpak::auto_remove()?;
-            }
-        }
-        "Install Packages" => run_install_packages(0, distribution)?,
-        "Exit" => return Ok(()),
-        _ => (),
+    if other::is_installed(package, distribution) {
+        return helper::get_colored_string("Other", Color::Yellow);
     }
-
-    run_menu(selection + 1, distribution)
-}
-
-fn main() -> Result<(), io::Error> {
-    let mut distribution = distribution::Distribution::new()?;
-    run_menu(0, &mut distribution)
+    return helper::get_colored_string("Uninstalled", Color::Red);
 }
